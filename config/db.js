fa155576c5e792145d96e6a2bad16ad89dd81d70
surajmendhe5573 <surajmendhe470@gmail.com>
2025-02-02 13:59:41 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -27,9 +27,9 @@ const db = mysql.createPool({ // Create a connection pool for better scalability
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0,
 });
 
 db.getConnection()
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -57,7 +57,7 @@ const login= async(req, res)=>{
 const getAllUsers = async (req, res) => {
     try {
         const users = await User.findAll();
-        res.json(users);
+        res.status(200).json({message: 'Users fetched successfully', users});
     } catch (err) {
         res.status(500).json({ message: 'Database error' });
     }
