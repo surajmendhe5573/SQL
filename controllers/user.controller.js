7f820be6007e25eeb68c52dbc2c2271c8a4c9123
surajmendhe5573 <surajmendhe470@gmail.com>
2025-02-02 14:29:48 +0530

    done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -91,7 +91,7 @@ const updateUser = async (req, res) => {
 
         res.json({ message: 'User updated successfully' });
     } catch (err) {
-        res.status(500).json({ message: 'Database error' });
+        res.status(500).json({ message: 'Internal server error' });
     }
 };
 
